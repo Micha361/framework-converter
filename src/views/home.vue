8603a34d26712ad337f837ba25683b839016a515
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2025-01-01 04:40:40 +0100

    optimizing frontent for zip download

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <h2>Convert youre project</h2>
+    <h2>Convert your project</h2>
     <select v-model="selectedFramework">
       <option value="vue">Vue.js</option>
       <option value="react">React</option>
     </select>
 
     <input type="file" accept=".zip" @change="onFileSelect" multiple />
-    <button @click="startConversion">convert</button>
+    <button @click="startConversion">Convert</button>
+    <button @click="downloadZip" v-if="conversionComplete">Download ZIP</button>
 
     <p v-if="message">{{ message }}</p>
   </div>
@@ -22,6 +23,7 @@ export default {
       selectedFramework: 'vue',
       selectedFiles: null,
       message: '',
+      conversionComplete: false,
     };
   },
   methods: {
@@ -32,9 +34,27 @@ export default {
       try {
         const response = await convertProject(this.selectedFiles, this.selectedFramework);
         this.message = response.message;
+        this.conversionComplete = true;
       } catch (error) {
         console.error(error);
-        this.message = 'An error acured while converting!';
+        this.message = 'An error occurred while converting!';
+      }
+    },
+    async downloadZip() {
+      try {
+        const response = await fetch('http://localhost:3000/download');
+        if (!response.ok) throw new Error('Download failed');
+
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'converted_project.zip';
+        a.click();
+        window.URL.revokeObjectURL(url);
+      } catch (error) {
+        console.error('Error downloading ZIP:', error);
+        this.message = 'An error occurred while downloading the ZIP file!';
       }
     },
   },
