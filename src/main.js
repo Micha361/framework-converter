ebe24894ba8c1421b3da12ab5f183e92c155a495
MICHAEL HURSCHLER <MICHAEL_HURSCHLER@sluz.ch>
2025-01-04 22:49:42 +0100

    update styles.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './style.css'; 
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/home.vue';
 import Info from './views/info.vue';
